0eefc9711711988b5af342bddee5eef13ed41dcb
Rakshitha <rakshithadilshan1@gmail.com>
2021-03-14 20:09:56 +0530

    search list filter for destination

--- PassengerApplication/tma_passenger/lib/screens/addride/destination.dart
+++ PassengerApplication/tma_passenger/lib/screens/addride/destination.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:tma_passenger/screens/addride/pickup.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:async/async.dart';
+
+// Init firestore and geoFlutterFire
+final geo = Geoflutterfire();
+final _firestore = FirebaseFirestore.instance;
+
 
 class SelectDestination extends StatelessWidget {
   @override
@@ -22,25 +31,10 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
 
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body:Map()
-    );
-  }
-}
+ TextEditingController _searchController = TextEditingController();
 
-class Map extends StatefulWidget {
-  @override
-  _MapState createState() => _MapState();
-}
-
-class  _MapState extends State<Map> {
-  GoogleMapController mapController;
-  static const _initialPosition = LatLng(7.2906, 80.6337);
-  LatLng _lastPostion = _initialPosition;
-  final Set<Marker> _markers = {};
 
   @override
   Widget build(BuildContext context) {
@@ -51,65 +45,53 @@ class  _MapState extends State<Map> {
         centerTitle: true,
       ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SelectPickup()),
-          );
-        },
-        child: Icon(Icons.arrow_forward_ios),
-        backgroundColor: Colors.black87,
-      ),
-
-      body: Stack(
+      body: Column(
         children: <Widget>[
-
-          GoogleMap(initialCameraPosition: CameraPosition(
-              target: _initialPosition, zoom: 200.0),
-            onMapCreated: onCreated,
-            myLocationEnabled: true,
-            mapType: MapType.normal,
-            compassEnabled: true,
-            markers: _markers,
-            onCameraMove: _onCameraMove,
-
-
+          TextField(
+            controller: _searchController,
+             decoration: InputDecoration(
+             prefixIcon:Icon(Icons.search),
+            ),
           ),
+        Expanded(
+          child:
 
+          StreamBuilder(
+
+
+              stream: FirebaseFirestore.instance.collection("cities").snapshots(),
+              builder: (context, snapshot) {
+                return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot cities = snapshot.data.docs[index];
+                    return ListTile(
+                      title: Text(cities['name']),
+                      subtitle: Text(cities['location']),
+                    );
+                  },
+
+                );
+              }
+          ),
+        ),
         ],
-      ),
+      ) ,
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.push(
+      //       context,
+      //       MaterialPageRoute(builder: (context) => SelectPickup()),
+      //     );
+      //   },
+      //   child: Icon(Icons.arrow_forward_ios),
+      //   backgroundColor: Colors.black87,
+      // ),
+
     );
   }
 
-  void onCreated(GoogleMapController controller) {
-    setState(() {
-      mapController = controller;
-    });
-  }
-
-
-  void _onCameraMove(CameraPosition position) {
-    setState(() {
-      _lastPostion = position.target;
-    });
-  }
-
-
-  void _onAddMarkerPressed() {
-    setState(() {
-      _markers.add(Marker(markerId: MarkerId(_lastPostion.toString()),
-          position: _lastPostion,
-          infoWindow: InfoWindow(
-            title: "remeber here",
-            snippet: "good place",
-
-          ),
-          icon: BitmapDescriptor.defaultMarker
-      ));
-    });
-  }
 }
-
 
 
