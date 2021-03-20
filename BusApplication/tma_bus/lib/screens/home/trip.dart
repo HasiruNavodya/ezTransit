df732deee62aca3c874325627e76c1cc797d6957
Hasiru Navodya <hasirunmp@gmail.com>
2021-03-21 01:57:34 +0530

    geofencing added

--- BusApplication/tma_bus/lib/screens/home/trip.dart
+++ BusApplication/tma_bus/lib/screens/home/trip.dart
@@ -2,7 +2,33 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 //import 'package:tma_bus/screens/trip/starttrip.dart';
 import 'package:location/location.dart';
+import 'package:tma_bus/screens/trip/starttrip.dart';
+import 'package:geofence_service/geofence_service.dart';
 
+final geofenceService = GeofenceService(
+    interval: 5000,
+    accuracy: 100,
+    allowMockLocations: true
+);
+
+final geofenceList = <Geofence>[
+  Geofence(
+      id: 'place_1',
+      latitude: 6.8408,
+      longitude: 79.9992,
+      radius: [
+        GeofenceRadius(id: 'radius_25m', length: 25),
+        GeofenceRadius(id: 'radius_100m', length: 100)
+      ]
+  ),
+];
+
+class TripCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 class TripControlView extends StatefulWidget {
   @override
@@ -13,11 +39,44 @@ class _TripControlViewState extends State<TripControlView> {
 
   String tripStatus = "off";
 
+  void onGeofenceStatusChanged(
+      Geofence geofence,
+      GeofenceRadius geofenceRadius,
+      GeofenceStatus geofenceStatus) {
+   // print('geofence: ${geofence.toMap()}');
+   // print('geofenceRadius: ${geofenceRadius.toMap()}');
+    print('geofenceStatus: ${geofenceStatus.toString()}\n');
+  }
+
+  void onActivityChanged(
+      Activity prevActivity,
+      Activity currActivity) {
+    print('prevActivity: ${prevActivity.toMap()}');
+    print('currActivity: ${currActivity.toMap()}\n');
+  }
+
+  void onError(dynamic error) {
+    final errorCode = getErrorCodesFromError(error);
+    if (errorCode == null) {
+      print('Undefined error: $error');
+      return;
+    }
+
+    print('ErrorCode: $errorCode');
+  }
+
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      geofenceService.setOnGeofenceStatusChanged(onGeofenceStatusChanged);
+      geofenceService.setOnActivityChanged(onActivityChanged);
+      geofenceService.setOnStreamError(onError);
+      geofenceService.start(geofenceList).catchError(onError);
+    });
     storeUserLocation();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,43 +98,18 @@ class _TripControlViewState extends State<TripControlView> {
     }
 
     else{
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Trip"),
-          backgroundColor: Colors.black,
-          centerTitle: true,
-        ),
-        //backgroundColor: Colors.red,
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            color: Colors.amber,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      );
+      return TripNav();
     }
   }
 }
 
-storeUserLocation()
-{
+storeUserLocation(){
 
   Location location = new Location();
 
   location.onLocationChanged.listen((LocationData currentLocation) {
-
     FirebaseFirestore.instance.collection('buses').doc('eg2345').set({
       'location' : GeoPoint(currentLocation.latitude, currentLocation.longitude)
     });
-
   });
 }
--- BusApplication/tma_bus/lib/screens/trip/starttrip.dart
+++ BusApplication/tma_bus/lib/screens/trip/starttrip.dart
@@ -1,22 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:tma_bus/screens/home/trip.dart';
 
-class SelectTrip extends StatelessWidget {
+class TripNav extends StatefulWidget {
+  @override
+  _TripNavState createState() => _TripNavState();
+}
+
+class _TripNavState extends State<TripNav> {
+
+  String trip = '1';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Select Trip"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+
+    if(trip=='1'){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("screen1"),
         ),
-      ),
-    );
+        body: Container(
+          child: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  trip = '2';
+                });
+              },
+              child: Text('screen2'),
+            ),
+          ),
+        ),
+      );
+    }
+
+    else{
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("screen2"),
+        ),
+        body: Container(
+          child: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                staetrfv();
+                setState(() {
+                  trip = '1';
+                });
+              },
+              child: Text('screen1'),
+            ),
+          ),
+        ),
+      );
+    }
+
   }
+}
+
+void staetrfv(){
+  print("asd");
 }
