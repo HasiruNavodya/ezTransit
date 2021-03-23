e7e11f1fc004278b8674c3c4a268398ef9cb1c3c
Rakshitha <rakshithadilshan1@gmail.com>
2021-03-23 21:09:16 +0530

    updated confirm.dart

--- PassengerApplication/tma_passenger/lib/screens/addride/confirm.dart
+++ PassengerApplication/tma_passenger/lib/screens/addride/confirm.dart
@@ -20,53 +20,123 @@ class ConfirmTicket extends StatelessWidget {
       //backgroundColor: Colors.red,
       body: Column(
 
-            crossAxisAlignment: CrossAxisAlignment.start,
+
             children: <Widget>[
+            Expanded(
+                flex: 1,
+              child:Container(
 
-              Text(
-                "Destination: $destinationloc",
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-              ),
-              Text(""),
+               child: Text("Destination: $destinationloc"),
 
-              Text(
-                "Pickup Location: $pickuploc",
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-              ),
-              Text(""),
-
-
-              Text(
-                "Bus:",
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-              ),
-              Text(""),
-
-              Text(
-                "Pick Up At:",
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-              ),
-              Text(""),
-
-              Text(
-                "Dropping At:",
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-              ),
-              Text(""),
-
-              Text(
-                "Ticket Price:",
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-              ),
-              FlatButton(color:Colors.green, onPressed: () {}, child: Text("Yes")
-              ),
-              RaisedButton(color:Colors.red, onPressed: () {}, child: Text("No")
-              ),
+              // Text(""),
+              //
+              // Text(
+              //   "Pickup Location: $pickuploc",
+              //   style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+              // ),
+              // Text(""),
+              //
+              //
+              // Text(
+              //   "Bus:",
+              //   style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+              // ),
+              // Text(""),
+              //
+              // Text(
+              //   "Pick Up At:",
+              //   style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+              // ),
+              // Text(""),
+              //
+              // Text(
+              //   "Dropping At:",
+              //   style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+              // ),
+              // Text(""),
+              //
+              // Text(
+              //   "Ticket Price:",
+              //   style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+              // ),
+              // FlatButton(color:Colors.green, onPressed: () {}, child: Text("Yes")
+              // ),
+              // RaisedButton(color:Colors.red, onPressed: () {}, child: Text("No")
+              // ),
 
 
+            ),
+        ),
 
-            ],
+              Expanded(
+               flex: 1,
+               child:Container(
+
+                  child: Text("Pickup Location: $pickuploc"),
+
           ),
+              ),
+
+      Expanded(
+        flex: 1,
+        child:Container(
+
+          child: Text("Bus:"),
+
+        ),
+      ),
+
+      Expanded(
+        flex: 1,
+        child:Container(
+
+          child: Text("Pick Up At:"),
+
+        ),
+      ),
+
+      Expanded(
+        flex: 1,
+        child:Container(
+
+          child: Text("Dropping At:"),
+
+        ),
+      ),
+
+      Expanded(
+        flex: 1,
+        child:Container(
+
+          child: Text("Ticket Price:"),
+
+        ),
+      ),
+
+              Expanded(
+                flex: 1,
+                child:Row(
+                  children: <Widget> [
+
+                             FlatButton(color:Colors.green, onPressed: () {}, child: Text("Yes"),
+                          ),
+                           RaisedButton(color:Colors.red, onPressed: () {}, child: Text("No")
+                         ),
+                    
+
+                  ],
+
+
+
+                ),
+              ),
+
+
+
+
+
+
+
 
 
       // floatingActionButton: FloatingActionButton(
@@ -79,6 +149,8 @@ class ConfirmTicket extends StatelessWidget {
       //   child: Icon(Icons.arrow_forward_ios),
       //   backgroundColor: Colors.black87,
       // ),
+      ],
+    ),
     );
   }
 }
