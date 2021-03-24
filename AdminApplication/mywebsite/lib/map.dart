cbfeb8e18f23e1beb6b84d53d26fb52b1b65ebb7
Prabodhidissanayake <54392255+Prabodhidissanayake@users.noreply.github.com>
2021-03-24 18:49:06 +0530

    Update map.dart

--- AdminApplication/mywebsite/lib/map.dart
+++ AdminApplication/mywebsite/lib/map.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 
-
 class GoogleMap extends StatefulWidget {
   @override
   _GoogleMapState createState() => _GoogleMapState();
 }
 
 class _GoogleMapState extends State<GoogleMap> {
+  List<Marker> allMarkers = [];
+
+  GoogleMapController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    allMarkers.add(Marker(
+        markerId: MarkerId('myMarker'),
+        draggable: true,
+        onTap: () {
+          print('Marker Tapped');
+        },
+        position: LatLng(40.7128, -74.0060)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition,
+      appBar: AppBar(
+        title: Text('Maps'),
+      ),
+      body: Stack(
+        children: [Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: GoogleMap(
+            initialCameraPosition:
+                CameraPosition(target: LatLng(40.7128, -74.0060), zoom: 12.0),
+            markers: Set.from(allMarkers),
+            onMapCreated: mapCreated,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: InkWell(
+            onTap: movetoBoston,
+            child: Container(
+              height: 40.0,
+              width: 40.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.green
+              ),
+              child: Icon(Icons.forward, color: Colors.white),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: InkWell(
+            onTap: movetoNewYork,
+            child: Container(
+              height: 40.0,
+              width: 40.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.red
+              ),
+              child: Icon(Icons.backspace, color: Colors.white),
+            ),
+          ),
+        )
+        ]
       ),
     );
+  }
+
+  void mapCreated(controller) {
+    setState(() {
+      _controller = controller;
+    });
+  }
+
+  movetoBoston() {
+    _controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: LatLng(42.3601, -71.0589), zoom: 14.0, bearing: 45.0, tilt: 45.0),
+    ));
+  }
+
+  movetoNewYork() {
+    _controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: LatLng(40.7128, -74.0060), zoom: 12.0),
+    ));
   }
 }
