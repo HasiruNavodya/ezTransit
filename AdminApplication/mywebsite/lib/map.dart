c08b7f27bc69a1836c1892b0418bc345dc8048b3
Prabodhidissanayake <54392255+Prabodhidissanayake@users.noreply.github.com>
2021-03-25 10:27:58 +0530

    map

--- AdminApplication/mywebsite/lib/map.dart
+++ AdminApplication/mywebsite/lib/map.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,6 +11,21 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
+Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+ 
+  //get marker 
+
+void getMarkers(double lat, double long) {
+    MarkerId markerId = MarkerId(lat.toString() + long.toString());
+    Marker marker = Marker(
+        markerId: markerId,
+        position: LatLng(lat, long),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+        infoWindow: InfoWindow(snippet: 'Address'));
+    setState(() {
+      markers[markerId] = marker;
+    });
+  } 
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(7.8731, 80.7718),
@@ -26,6 +42,17 @@ class MapSampleState extends State<MapSample> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
+
+        onTap: (tapped) async {
+                    getMarkers(tapped.latitude, tapped.longitude);
+                    await FirebaseFirestore.instance
+                        .collection('Marker_location')
+                        .add({
+                      'latitude': tapped.latitude,
+                      'longitude': tapped.longitude,
+                    });
+                  },
+
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
