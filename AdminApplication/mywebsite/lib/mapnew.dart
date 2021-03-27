1882053172c6e1ede9fc917770a6c1bbfc1c6c62
Hasiru Navodya <hasirunmp@gmail.com>
2021-03-27 15:05:02 +0530

    mapnew update

--- AdminApplication/mywebsite/lib/mapnew.dart
+++ AdminApplication/mywebsite/lib/mapnew.dart
@@ -96,45 +96,6 @@ class _MapClickBodyState extends State<_MapClickBody> {
           ),
         ],
       ),
-
-    );
-    final GoogleMap googleMap = GoogleMap(
-      onMapCreated: onMapCreated,
-      initialCameraPosition: _kInitialPosition,
-      onTap: (LatLng pos) {
-        setState(() {
-          _lastTap = pos;
-        });
-      },
-      onLongPress: (LatLng pos) {
-        setState(() {
-          _lastLongPress = pos;
-        });
-      },
-    );
-
-    final List<Widget> columnChildren = <Widget>[
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Center(
-          child: SizedBox(
-            width: 300.0,
-            height: 200.0,
-            child: googleMap,
-          ),
-        ),
-      ),
-    ];
-
-    if (mapController != null) {
-      final String lastTap = 'Tap:\n${_lastTap ?? ""}\n';
-      columnChildren.add(Center(child: Text(lastTap, textAlign: TextAlign.center)));
-    }
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: columnChildren,
     );
   }
 
