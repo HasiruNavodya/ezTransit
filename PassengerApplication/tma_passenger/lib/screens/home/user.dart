113f6319a00a42f772e7a5892b0f3c63c5a73e70
Rakshitha <rakshithadilshan1@gmail.com>
2021-03-28 01:47:43 +0530

    .

--- PassengerApplication/tma_passenger/lib/screens/home/user.dart
+++ PassengerApplication/tma_passenger/lib/screens/home/user.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class UserDetails extends StatelessWidget {
@@ -11,120 +12,148 @@ class UserDetails extends StatelessWidget {
       ),
       //backgroundColor: Colors.red,
       backgroundColor: Colors.white70,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage('assets/images.png'),
+      body:StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("passengers")
+            .where('Email', isEqualTo: 'hasiru@gmail.com').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
 
-              ),
-              Text(
-                '',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 25,
-                ),
-              ),
-              Text(
-                'Welcome',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'SourceSansPro',
-                  color: Colors.black87,
-                  letterSpacing: 2.5,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 200,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
 
-              Card(
-                  color: Colors.white,
-                  margin:
-                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.account_box,
-                      color: Colors.teal[900],
-                    ),
-                    title: Text(
-                      'Name',
-                      style:
-                      TextStyle(fontFamily: 'BalooBhai', fontSize: 15.0,fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text('Rakshitha Dilshan',
-                      style: TextStyle(
-                      fontSize: 14,
-                      //fontWeight: FontWeight.bold,
-                      //fontFamily: 'SourceSansPro',
-                      color: Colors.black87,
-                      //letterSpacing: 2.5,
-                    ),
-                    ),
-                  )),
-              Card(
-                color: Colors.white,
-                margin:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal[900],
-                  ),
-                  title: Text(
-                    'Email',
-                    style: TextStyle(fontSize: 15.0, fontFamily: 'Neucha',fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text('rakshihtadilshna1@gmail.com',
-                    style: TextStyle(
-                      fontSize: 14,
-                      //fontWeight: FontWeight.bold,
-                      //fontFamily: 'SourceSansPro',
-                      color: Colors.black87,
-                      //letterSpacing: 2.5,
-                    ),
-                  ),
-                ),
-              ),
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                return Column(
 
-              Card(
-                color: Colors.white,
-                margin:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.perm_identity,
-                    color: Colors.teal[900],
-                  ),
-                  title: Text(
-                    'NIC',
-                    style: TextStyle(fontSize: 15.0, fontFamily: 'Neucha',fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text('34242532534',
-                    style: TextStyle(
-                      fontSize: 14,
-                      //fontWeight: FontWeight.bold,
-                      //fontFamily: 'SourceSansPro',
-                      color: Colors.black87,
-                      //letterSpacing: 2.5,
-                    ),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 80,
+                          backgroundImage: AssetImage('assets/images.png'),
 
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+                        ),
+                        Text(
+                          '',
+                          style: TextStyle(
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 25,
+                          ),
+                        ),
+                        Text(
+                          'Welcome',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'SourceSansPro',
+                            color: Colors.black87,
+                            letterSpacing: 2.5,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                          width: 200,
+                          child: Divider(
+                            color: Colors.teal[100],
+                          ),
+                        ),
+
+
+                        Card(
+                            color: Colors.white,
+                            margin:
+                            EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.account_box,
+                                color: Colors.teal[900],
+                              ),
+                              title: Text(
+                                'Name',
+                                style:
+                                TextStyle(fontFamily: 'BalooBhai',
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('${document.data()['FullName']}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  //fontWeight: FontWeight.bold,
+                                  //fontFamily: 'SourceSansPro',
+                                  color: Colors.black87,
+                                  //letterSpacing: 2.5,
+                                ),
+                              ),
+                            )),
+                        Card(
+                          color: Colors.white,
+                          margin:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.email,
+                              color: Colors.teal[900],
+                            ),
+                            title: Text(
+                              'Email',
+                              style: TextStyle(fontSize: 15.0,
+                                  fontFamily: 'Neucha',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text('${document.data()['Email']}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                //fontWeight: FontWeight.bold,
+                                //fontFamily: 'SourceSansPro',
+                                color: Colors.black87,
+                                //letterSpacing: 2.5,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        Card(
+                          color: Colors.white,
+                          margin:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.perm_identity,
+                              color: Colors.teal[900],
+                            ),
+                            title: Text(
+                              'NIC',
+                              style: TextStyle(fontSize: 15.0,
+                                  fontFamily: 'Neucha',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text('${document.data()['NIC']}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                //fontWeight: FontWeight.bold,
+                                //fontFamily: 'SourceSansPro',
+                                color: Colors.black87,
+                                //letterSpacing: 2.5,
+                              ),
+
+                            ),
+                          ),
+                        ),
+
+                      ],
+
+
+                );
+                //Card(child: Text(document.data()['name']??'default'),);
+              }).toList(),
+            ),
+          );
+        },
       ),
-
     );
   }
 }
