a2781d019943d36844b210f07269bea9d124510e
Ravindu Jayathilaka <57854539+Ravindu-Jayathilaka@users.noreply.github.com>
2021-03-31 23:21:41 +0530

    Update confirm.dart

--- PassengerApplication/tma_passenger/lib/screens/addride/confirm.dart
+++ PassengerApplication/tma_passenger/lib/screens/addride/confirm.dart
@@ -1,11 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:tma_passenger/screens/addride/buy.dart';
+import 'package:payhere_mobilesdk_flutter/payhere_mobilesdk_flutter.dart';
+import 'package:toast/toast.dart';
 
 import 'destination.dart';
-String status='no';
 
-class ConfirmTicket extends StatefulWidget {
+class ConfirmTicket extends StatelessWidget {
   String destinationloc;
   String pickuploc;
   String bus;
@@ -13,91 +13,101 @@ class ConfirmTicket extends StatefulWidget {
   String tripid;
   String startcity;
   String endcity;
+  String pickupat;
+  String droppingat;
 
-  ConfirmTicket(dloc, ploc, bs, ticketprice,tripid,startcity,endcity) {
+  ConfirmTicket(dloc, ploc, bs, ticketprice, tripid, startcity, endcity) {
     this.destinationloc = dloc;
     this.pickuploc = ploc;
     this.bus = bs;
     this.ticketprice = ticketprice;
-    this.tripid=tripid;
-    this.startcity=startcity;
-    this.endcity=endcity;
+    this.tripid = tripid;
+    this.startcity = startcity;
+    this.endcity = endcity;
   }
 
-  @override
-  _ConfirmTicketState createState() => _ConfirmTicketState();
-}
-
-class _ConfirmTicketState extends State<ConfirmTicket> {
-  String pickupat;
-
-  String droppingat;
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-   if(status=='no')
-     {
-       getPickupAt();
-       getDroppingAt();
-       print('fdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasffffffffffffffff');
-     }
-
-
-
-
-    });
-
-  }
-
-  void getPickupAt(){
+  void getPickupAt() {
     FirebaseFirestore.instance
         .collection("trips")
-        .doc('${widget.tripid}')
-        .collection("stops").doc('${widget.startcity}')
+        .doc('$tripid')
+        .collection("stops")
+        .doc('$startcity')
         .get()
-        .then((documentSnapshot)  {
+        .then((documentSnapshot) {
       if (documentSnapshot.exists) {
-        pickupat=documentSnapshot.data()['time'];
+        pickupat = documentSnapshot.data()['time'];
         print('fffff $pickupat');
 
-
-        print('${widget.startcity}');
-        print('${widget.endcity}');
-
-
-
-
-
+        print('$startcity');
+        print('$endcity');
       }
-      setState(() {});
     });
-
   }
 
-  void getDroppingAt(){
+  void getDroppingAt() {
     FirebaseFirestore.instance
         .collection("trips")
-        .doc('${widget.tripid}')
-        .collection("stops").doc('${widget.endcity}')
+        .doc('$tripid')
+        .collection("stops")
+        .doc('$endcity')
         .get()
-        .then((documentSnapshot)  {
+        .then((documentSnapshot) {
       if (documentSnapshot.exists) {
-        droppingat=documentSnapshot.data()['time'];
+        droppingat = documentSnapshot.data()['time'];
         print('ggggg $droppingat');
-        print('${widget.startcity}');
-        print('${widget.endcity}');
-
-
-
+        print('$startcity');
+        print('$endcity');
       }
-      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    getPickupAt();
+    getDroppingAt();
+
+    PayHerePay() {
+      Map paymentObject = {
+        "sandbox": true, // true if using Sandbox Merchant ID
+        "merchant_id": "1216958", // Replace your Merchant ID
+        "merchant_secret":
+            "4q6h8wJ4gl74ZJ3DAH0GEm8m0UeEP0Hk14pCBjyrOCYg", // See step 4e
+        "notify_url": "http://sample.com/notify",
+        "order_id": "ItemNo12345",
+        "items": "Hello from Flutter!",
+        "amount": ticketprice,
+        "currency": "LKR",
+        "first_name": "Saman",
+        "last_name": "Perera",
+        "email": "samanp@gmail.com",
+        "phone": "0771234567",
+        "address": "No.1, Galle Road",
+        "city": "Colombo",
+        "country": "Sri Lanka",
+        "delivery_address": "No. 46, Galle road, Kalutara South",
+        "delivery_city": "Kalutara",
+        "delivery_country": "Sri Lanka",
+        "custom_1": "",
+        "custom_2": ""
+      };
+
+      PayHere.startPayment(paymentObject, (paymentId) {
+        print("One Time Payment Success. Payment Id: $paymentId");
+      }, (error) {
+        print("One Time Payment Failed. Error: $error");
+        Toast.show(
+          "Payment Failed! Try Again.",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.CENTER,
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+          border: Border.all(color: Colors.white),
+        );
+      }, () {
+        print("One Time Payment Dismissed");
+      });
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -107,9 +117,10 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
       ),
       //backgroundColor: Colors.red,
       backgroundColor: Colors.white,
-      body: StreamBuilder<QuerySnapshot >(
+      body: StreamBuilder<QuerySnapshot>(
         // stream:FirebaseFirestore.instance.collection('trips').where('parts', isEqualTo: ).snapshots(),
-        stream: FirebaseFirestore.instance.collection('trips')
+        stream: FirebaseFirestore.instance
+            .collection('trips')
             // .doc('$tripid')
             // .collection('stops').doc('Kollupitiya').collection('gg')
             .snapshots(),
@@ -128,26 +139,20 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
               children: snapshot.data.docs.map((DocumentSnapshot document) {
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
-
-
                   children: <Widget>[
-
-
                     Expanded(
                       flex: 1,
                       child: ListTile(
                         leading: Icon(
                           Icons.add_road,
-                          color: Colors.black87,
+                          color: Colors.teal[900],
                         ),
-                        title: Text("Destination: ${widget.destinationloc}",
+                        title: Text(
+                          "Destination: $destinationloc",
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
-
                         ),
                       ),
-
-
                     ),
 
                     Expanded(
@@ -155,13 +160,13 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                       child: ListTile(
                         leading: Icon(
                           Icons.edit_road_outlined,
-                          color: Colors.black87,
+                          color: Colors.teal[900],
                         ),
-                        title: Text("Pickup Location: ${widget.pickuploc}",
+                        title: Text(
+                          "Pickup Location: $pickuploc",
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-
                       ),
                     ),
 
@@ -170,15 +175,14 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                       child: ListTile(
                         leading: Icon(
                           Icons.directions_bus_outlined,
-                          color: Colors.black87,
+                          color: Colors.teal[900],
                         ),
-                        title: Text("Bus: ${widget.bus} ",
+                        title: Text(
+                          "Bus: $bus ",
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-
                       ),
-
                     ),
 
                     Expanded(
@@ -186,9 +190,10 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                       child: ListTile(
                         leading: Icon(
                           Icons.timer_rounded,
-                          color: Colors.black87,
+                          color: Colors.teal[900],
                         ),
-                        title: Text('Pickup At: $pickupat',
+                        title: Text(
+                          "Pick Up At: $pickupat",
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
@@ -200,16 +205,14 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                       child: ListTile(
                         leading: Icon(
                           Icons.access_time_outlined,
-                          color: Colors.black87,
+                          color: Colors.teal[900],
                         ),
-                        title: Text('Dropping At: $droppingat',
+                        title: Text(
+                          "Dropping At: $droppingat",
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-
                       ),
-
-
                     ),
 
                     Expanded(
@@ -217,15 +220,15 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                       child: ListTile(
                         leading: Icon(
                           Icons.credit_card_sharp,
-                          color: Colors.black87,
+                          color: Colors.teal[900],
                         ),
-                        title: Text("Ticket Price: Rs.${widget.ticketprice}",
+                        title: Text(
+                          "Ticket Price: Rs.$ticketprice",
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
-
 
                     Expanded(
                       flex: 1,
@@ -236,47 +239,40 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                             minWidth: 100.0,
                             height: 50.0,
                             child: RaisedButton(
-                              color: Colors.black, onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => BuyTicket()),
-                              );
-                            }, child: Text("Yes",
-                              style: TextStyle(
-                                color: Colors.white,
+                              color: Colors.black,
+                              onPressed: () => {PayHerePay()},
+                              child: Text(
+                                "Yes",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                            ),
                           ),
-
-
                           ButtonTheme(
                             minWidth: 100.0,
                             height: 50.0,
                             child: RaisedButton(
-                              color: Colors.black, onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SelectDestination()),
-                              );
-                            }, child: Text("No",
-                              style: TextStyle(
-                                color: Colors.white,
+                              color: Colors.black,
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          SelectDestination()),
+                                );
+                              },
+                              child: Text(
+                                "No",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
-
-                            ),
                             ),
                           ),
-
-
                         ],
-
-
                       ),
                     ),
-
 
                     // floatingActionButton: FloatingActionButton(
                     //   onPressed: () {
@@ -289,18 +285,13 @@ class _ConfirmTicketState extends State<ConfirmTicket> {
                     //   backgroundColor: Colors.black87,
                     // ),
                   ],
-
-
                 );
 
                 //Card(child: Text(document.data()['name']??'default'),);
               }).toList(),
-
             ),
-
           );
         },
-
       ),
     );
   }
