0eb11f6e933b78c315b3078e031c7c57b87f0be3
Hasiru Navodya <hasirunmp@gmail.com>
2021-05-30 06:13:33 +0530

    bus app home screen

--- BusApplication/tma_bus/lib/screens/home/home.dart
+++ BusApplication/tma_bus/lib/screens/home/home.dart
@@ -4,9 +4,8 @@ import 'package:tma_bus/screens/home/account.dart';
 import 'package:tma_bus/screens/home/map.dart';
 import 'package:tma_bus/screens/home/report.dart';
 import 'package:tma_bus/screens/home/addtrip.dart';
-import 'package:tma_bus/screens/trip/trip.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:location/location.dart';
+import 'package:intl/intl.dart';
 //import 'package:permission_handler/permission_handler.dart';
 
 class HomeView extends StatefulWidget {
@@ -20,7 +19,7 @@ class _HomeViewState extends State<HomeView> with SingleTickerProviderStateMixin
   @override
   void initState() {
     super.initState();
-    controller = TabController(length: 4, vsync: this);
+    controller = TabController(length: 3, vsync: this, initialIndex: 1);
     locationPermission();
   }
 
@@ -35,7 +34,7 @@ class _HomeViewState extends State<HomeView> with SingleTickerProviderStateMixin
     return Scaffold(
       body: TabBarView(
         physics: NeverScrollableScrollPhysics(),
-        children: <Widget>[MapView(), AddTripView(), ReportEmergencyView(), Account()],
+        children: <Widget>[MapView(), HomeScreen(), Account()],
         controller: controller,
       ),
       bottomNavigationBar: Material(
@@ -47,9 +46,6 @@ class _HomeViewState extends State<HomeView> with SingleTickerProviderStateMixin
             ),
             Tab(
               icon: Icon(Icons.directions_bus),
-            ),
-            Tab(
-              icon: Icon(Icons.report_problem),
             ),
             Tab(
               icon: Icon(Icons.person),
@@ -87,7 +83,125 @@ class _HomeViewState extends State<HomeView> with SingleTickerProviderStateMixin
 
     _locationData = await location.getLocation();
   }
+  
+}
 
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ezTransit"),
+        backgroundColor: Colors.black87,
+        centerTitle: true,
+      ),
+      //backgroundColor: Colors.red,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/mapbg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Opacity(
+                  opacity: 0.8,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: StreamBuilder(
+                        stream: Stream.periodic(const Duration(seconds: 1)),
+                        builder: (context, snapshot) {
+                          return Center(
+                            child: Opacity(
+                              opacity: 1.0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  DateFormat().add_jms().format(DateTime.now()),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 40,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //SizedBox(height: 160,),
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => AddTripView()),);
+                      },
+                      color: Colors.black87,
+                      textColor: Colors.white,
+                      child: Icon(
+                        Icons.play_arrow,
+                        size:40,
+                        color: Colors.yellow,
+                      ),
+                      padding: EdgeInsets.all(16),
+                      shape: CircleBorder(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        "START NEW TRIP",
+                        style: TextStyle(
+                          fontSize: 20,
+                          //fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton.icon(
+                      label: Text("REPORT PROBLEM",
+                        style: TextStyle(
+                          //fontSize: 20,
+                          //fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      icon: Icon(Icons.report_problem,color: Colors.red,),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => ReportEmergencyView()),);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
